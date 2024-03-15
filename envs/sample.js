1f9ef8be4019031fe9be1ad001a770f88e5bbbe5
rohit <rkg10072000@gmail.com>
2024-03-15 11:33:28 +0530

    updated sample env file

--- envs/sample.js
+++ envs/sample.js
@@ -1,10 +1,11 @@
 export default {
-  "type": "json",
-  "HOST": "localhost",
-  "PORT": 5000,
+  HOST: "localhost",
+  PORT: 5000,
 
-  "JWT_SECRET_KEY": "abcd",
-  "JWT_EXPIRY_TIME": "1d",
+  JWT_SECRET_KEY: "abcd",  // secret key to sign access token
+  JWT_EXPIRY_TIME: "1d",  // expiry time for access token in days
 
-  "HASHING_SALT_ROUDS": 10
-}
+  HASHING_SALT_ROUDS: 10,  // number of salt rounds for hashing
+
+  MONGO_URI: "<YOUR MONGODB DATABASE CONNECTION STRING>",
+};
