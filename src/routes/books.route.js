8f398bebe4962c84c6cb5dc1c511ad7fab536ea5
rohit <rkg10072000@gmail.com>
2024-03-15 12:24:03 +0530

    add book validation

--- src/routes/books.route.js
+++ src/routes/books.route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import * as bookController from "../controllers/books.controller.js";
 import { asyncWrapper } from "../utils/async-wrapper.util.js";
+import { validateBody } from "../middlewares/validation.middleware.js";
+import { addBookSchema } from "../validations/book.schema.js";
 
 export const bookRouter = Router();
 
-bookRouter.post("/", asyncWrapper(bookController.addBook));
+bookRouter.post(
+  "/",
+  validateBody(addBookSchema),
+  asyncWrapper(bookController.addBook)
+);
 bookRouter.get("/", asyncWrapper(bookController.getAllBooks));
 bookRouter.get("/:isbn", asyncWrapper(bookController.getBook));
---
+++ src/validations/book.schema.js
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export const addBookSchema = Joi.object({
+  title: Joi.string().min(3).max(100).required().messages({
+    "string.min": "Title should have minimum length of 3.",
+    "string.max": "Title should have maximum length of 100",
+    "any.required": "Title is required",
+    "string.base": "Title should be a string",
+  }),
+  author: Joi.string().min(1).max(100).required().messages({
+    "string.min": "Author should have minimum length of 1.",
+    "string.max": "Author should have maximum length of 100",
+    "any.required": "Author is required",
+    "string.base": "Author should be a string",
+  }),
+  isbn: Joi.string().min(5).max(5).required().messages({
+    "string.min": "ISBN should have length of 5.",
+    "string.max": "ISBN should have length of 5",
+    "any.required": "ISBN is required",
+    "string.base": "ISBN should be a string",
+  }),
+  quantity: Joi.number().min(1).integer().messages({
+    "number.base": "Quantity should a number",
+    "number.min": "Quantity should be minimum 1",
+    "number.integer": "Quantity should be a integer",
+  }),
+});
